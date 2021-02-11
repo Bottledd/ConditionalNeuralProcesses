fixed training and variance

1D_Regession/ConditionalNeuralProcess.py
@@ -28,6 +28,7 @@ class ConditionalNeuralProcess(Model):
         # want distribution of all target points
         targets = tf.cast(targets, dtype=tf.float32)
         assert targets.dtype == means.dtype
+        # TODO change to multivariate for images
         dist = tfp.distributions.Normal(loc=means, scale=stds)
         log_prob = dist.log_prob(targets)
         #log_prob_per_batch = tf.reduce_mean(log_prob)
@@ -57,7 +58,7 @@ class Encoder(Layer):
 
     def call(self, inputs):
         """
-        Inputs is tuple (x_context, y_context, x_data), each shape [batch_size , num_points]
+        Inputs is tuple (x_context, y_context, x_data), each shape [batch_size , num_points, dimension]
         Need to reshape inputs to be pairs of [x_context, y_context],
         Pass through NN,
         Compute a representation by aggregating the outputs.
@@ -128,7 +129,9 @@ class Decoder(keras.layers.Layer):
         # split into 2 tensors, one with means and one with stds
         # floor the variance to avoid pathological solutions
         means, log_stds = tf.split(h, 2, axis=-1)
-        stds = 0.1 + 0.9 * tf.nn.softplus(log_stds)
+        stds = 0.01 + 0.99 * tf.nn.softplus(log_stds)
+        #stds = tf.nn.softplus(log_stds)
+
 
         return means, stds
 

1D_Regession/run_cnp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import collections
 import matplotlib.pyplot as plt
 from GaussianProcessSampler import GaussianProcess
@@ -8,17 +9,17 @@ import os
 import time
 
 
-def train(cnp, max_iters=30000):
+def train(cnp, max_iters=100000):
     # tf.config.run_functions_eagerly(True)
     gp_train = GaussianProcess(64, 10, testing=False)
     loss = []
     start = time.perf_counter()
     for i in range(1, max_iters):
-        # TODO: Change this to a generate_saples function so we can apply it to image data
+        # TODO: Change this to a generate_samples function so we can apply it to image data
         data_train = gp_train.generate_curves()
         loss.append(cnp.train_step(data_train.Inputs, data_train.Targets))
-        if i == 1 or i % 100 == 0:
-            print(f"Running avg (50) loss at iteration {i} is: {np.mean(loss[i-50:])}")
+        if i == 1 or i % 1000 == 0:
+            print(f"Running avg (50) loss at iteration {i} is: {np.mean(loss[i-1000:])}")
         # if i > 5000:
         #     # check any progress actually being made
         #     # (just to make computationally less expensive)
@@ -31,18 +32,21 @@ def train(cnp, max_iters=30000):
 if __name__ == "__main__":
     load = True
     save = True
-    saved_path = os.path.join(os.getcwd(), "saved_models/new")
+    training = True
+    saving_path = os.path.join(os.getcwd(), "saved_models/low_var")
+    loading_path = os.path.join(os.getcwd(), "saved_models/")
     cnp = ConditionalNeuralProcess(128)
     if load:
-        cnp.load_weights(saved_path)
-
-    cnp, loss, total_runtime = train(cnp)
-    print(total_runtime)
+        cnp.load_weights(loading_path)
+    if training:
+        cnp, loss, total_runtime = train(cnp)
+        print(total_runtime)
+        avg_loss = pd.Series(loss).rolling(window=1000).mean().iloc[1000 - 1:].values
+        plt.figure('loss')
+        plt.plot(avg_loss)
+        plt.show()
     if save:
-        cnp.save_weights(saved_path)
-    plt.figure('loss')
-    plt.plot(loss)
-    plt.show()
+        cnp.save_weights(saving_path)
 
     gp = GaussianProcess(1, 10, testing=True)
     data = gp.generate_curves()